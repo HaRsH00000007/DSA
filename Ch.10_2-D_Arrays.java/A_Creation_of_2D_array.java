445823bd89d0e781b5cf21bbb4adc058068af521
Harsh0.0.7 <124857047+HaRsH00000007@users.noreply.github.com>
2023-10-05 18:29:11 +0530

    Add files via upload

---
+++ Ch.10_2-D_Arrays.java/A_Creation_of_2D_array.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class A_Creation_of_2D_array {
+
+    public static boolean Search(int matrix[][], int key) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == key) {
+                    System.out.println("Found at cell  ( " + i + "," + j+")");
+                    return true;
+                }
+
+            }
+        }
+        System.out.println("Element not found at cell");
+        return false;
+    }
+
+    public static void main(String args[]) {
+        int matrix[][] = new int[3][3];
+        int n = matrix.length, m = matrix[0].length;
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        // Output
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+
+            System.out.println();
+        }
+        Search(matrix,5);
+    }
+    
+}
